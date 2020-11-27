ad020403e15fece699d4d20edfb2ed9724c20cec
Sravya2007 <sravyamarepally@outlook.com>
2020-11-27 17:45:41 +0530

    Created a new Expo app

---
+++ screens/BookTransactionScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import {Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
+
+export default BookTransactionScreen extends React.Component {
+    render() {
+        return (
+            <Text>Issue or Return</Text>
+        )
+    }
+}
---
+++ screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import {Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
+
+export default SearchScreen extends React.Component {
+    render() {
+        return (
+            <Text></Text>
+        )
+    }
+}
